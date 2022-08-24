3c4aec8200db91ea9fa1f0328ed2a48aabe82489
chicoikr <chicoikr@gmail.com>
2022-08-25 03:53:13 +0700

    Working but not perfect

--- app/src/main/java/com/chicoikr/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/chicoikr/simplecalculator/MainActivity.kt
@@ -82,21 +82,63 @@ class MainActivity : AppCompatActivity() {
                     tvValue = tvValue.substring(1)
                 }
                 if (tvValue.contains("-")){
-                val splitValue = tvValue.split("-")
-                var one = splitValue[0]
-                var two = splitValue[1]
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
 
-                if (prefix.isNotEmpty()){
-                    one = prefix + one
-                }
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
 
-                tvInput?.text = (one.toDouble() - two.toDouble()).toString()}
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())}
+                else if (tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
 
-            }catch (e: ArithmeticException){
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())}
+
+                else if (tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())}
+                else if (tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())}
+
+
+
+            }
+
+            catch (e: ArithmeticException){
                 e.printStackTrace()
             }
         }
 
+    }
+    fun removeZeroAfterDot(result: String) : String{
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0, result.length -2)
+
+        return value
     }
 
 }
